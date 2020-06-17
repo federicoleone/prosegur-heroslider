e6384e7df05ec5f516b6ff9fba852ede05c314bb
Federico Leone <32718106+federicoleone@users.noreply.github.com>
2020-06-17 12:31:49 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(function () {
+	/* Header drop down*/
+	$('#personas').mouseover(function () {
+		$('#menuPersonas').slideDown();
+		$('body').addClass('body-no-scroll');
+	});
+
+	$('#menuPersonas').find('#close-menu').click(function(){
+		$('#menuPersonas').hide();
+		$('body').removeClass('body-no-scroll');
+	});
+
+
+	/* Scroll Link */
+	$(document).on('click', 'header a[href^="#"], main a[href^="#"]', function (
+		event
+	) {
+		event.preventDefault();
+
+		$('html, body').animate(
+			{
+				scrollTop: $($.attr(this, 'href')).offset().top,
+			},
+			500
+		);
+	});
+});
